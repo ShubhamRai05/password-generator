db838b7f71cf46b5ad1e22121cdf0a63304ac9c8
Shubham Rai <shubham56237@gmail.com>
2023-08-02 00:20:03 +0530

    added comment on js file

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const lowerCase = "abcdefghijklmnopqrstuvwxyz"
 const numberSet ="1234567890"
 const symbolSet = "!@#$%^&*()-+*/"
 
+// adding selectors
+
 const getRandomData = (dataSet)=>{
     return dataSet[Math.floor(Math.random() * dataSet.length)]
 }
@@ -11,5 +13,5 @@ const generatePassword = (password="")=>{
     password = getRandomData(upperCase) + getRandomData(lowerCase) +getRandomData(numberSet) + getRandomData(symbolSet)
     return password
 }
-
+ 
 generatePassword()
